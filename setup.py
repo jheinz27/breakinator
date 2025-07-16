de5ba61c410f56e84d612c6f098030bcee2e9e29
jheinz27 <jheinz@g.harvard.edu>
2025-07-16 13:41:54 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="breakinator",
+    version="1.0.0",
+    py_modules=["breakinator"],
+    install_requires=[],  # add any runtime deps here, e.g. ["biopython"]
+)
